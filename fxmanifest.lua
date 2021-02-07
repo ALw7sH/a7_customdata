e0878198dc3e8697e477df8aadd08028a2e3c037
Alooy yousif <alooy16@gmail.com>
2021-02-07 14:18:40 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 	'cerulean'
+game 		'gta5'
+
+version 	'1.0.0'
+author 		' majqqq, ALw7sH <https://aliysf.com/>'
+
+server_scripts {
+	'server_main.lua'
+}
+
+client_scripts {
+	'client_main.lua'
+}
